fb747bd13ad70c57a7555b15a7cef024c335cdfb
Kendalynn Morris <kendalynn.morris@pnnl.gov>
2024-03-25 16:08:11 -0400

    change "protected" to "coupled"

--- read_in_landcalcs.R
+++ read_in_landcalcs.R
@@ -78,63 +78,63 @@ rm(uc_plot_data_long)
 
 #all things protected should become "Coupled"
 #fully coupled data (protected = TRUE,  spatially resolved = TRUE, coupled = TRUE)
-pro_AG_emissions <- read.csv("Feb24_set3of5/ag_emiss_Coupled_pro_newBeta_newQ10.csv", row.names = 1)
-pro_BG_emissions <- read.csv("Feb24_set3of5/bg_emiss_Coupled_pro_newBeta_newQ10.csv", row.names = 1)
-# pro_climate_data <- read.csv("Feb24_set3of5/climate_data_Coupled_pro_newBeta_newQ10.csv")
-# pro_gcam_land <- read.csv("Feb24_set3of5/gcam_land_alloc.csv")
-pro_leaf_data <- read.csv("Feb24_set3of5/leaf_data_Coupled_pro_newBeta_newQ10.csv")
-# pro_leaf_params <- read.csv("Feb24_set3of5/leaf_params_Coupled_pro_newBeta_newQ10.csv")
+coup_AG_emissions <- read.csv("Feb24_set3of5/ag_emiss_Coupled_pro_newBeta_newQ10.csv", row.names = 1)
+coup_BG_emissions <- read.csv("Feb24_set3of5/bg_emiss_Coupled_pro_newBeta_newQ10.csv", row.names = 1)
+# coup_climate_data <- read.csv("Feb24_set3of5/climate_data_Coupled_pro_newBeta_newQ10.csv")
+# coup_gcam_land <- read.csv("Feb24_set3of5/gcam_land_alloc.csv")
+coup_leaf_data <- read.csv("Feb24_set3of5/leaf_data_Coupled_pro_newBeta_newQ10.csv")
+# coup_leaf_params <- read.csv("Feb24_set3of5/leaf_params_Coupled_pro_newBeta_newQ10.csv")
 
 # transform bg emissions to format able to be joined with other leaf data
-pro_BG <- data.frame(t(pro_BG_emissions))
-colnames(pro_BG) <- row.names(pro_BG_emissions)
-rm(pro_BG_emissions)
-pro_BG$year <- seq(year0,last_year)
-pro_BG_final <- pro_BG %>% tidyr::pivot_longer(cols=-c("year"),
+coup_BG <- data.frame(t(coup_BG_emissions))
+colnames(coup_BG) <- row.names(coup_BG_emissions)
+rm(coup_BG_emissions)
+coup_BG$year <- seq(year0,last_year)
+coup_BG_final <- coup_BG %>% tidyr::pivot_longer(cols=-c("year"),
                                                names_to = "name",
                                                values_to="bg_emiss")
-rm(pro_BG)
+rm(coup_BG)
 
 # same for ag emissions
-pro_AG <- data.frame(t(pro_AG_emissions))
-colnames(pro_AG) <- row.names(pro_AG_emissions)
-rm(pro_AG_emissions)
-pro_AG$year <- seq(year0,last_year)
-pro_AG_final <- pro_AG %>% tidyr::pivot_longer(cols=-c("year"),
+coup_AG <- data.frame(t(coup_AG_emissions))
+colnames(coup_AG) <- row.names(coup_AG_emissions)
+rm(coup_AG_emissions)
+coup_AG$year <- seq(year0,last_year)
+coup_AG_final <- coup_AG %>% tidyr::pivot_longer(cols=-c("year"),
                                                names_to = "name",
                                                values_to="ag_emiss")
-rm(pro_AG)
+rm(coup_AG)
 
 # combine
-pro_leaf_data %>% select(-X) %>%
-  left_join(pro_BG_final, by = c("year", "name")) %>%
-  left_join(pro_AG_final, by = c("year", "name")) -> pro_plot_data
-rm(pro_leaf_data)
-rm(pro_AG_final)
-rm(pro_BG_final)
+coup_leaf_data %>% select(-X) %>%
+  left_join(coup_BG_final, by = c("year", "name")) %>%
+  left_join(coup_AG_final, by = c("year", "name")) -> coup_plot_data
+rm(coup_leaf_data)
+rm(coup_AG_final)
+rm(coup_BG_final)
 
 # calculate total net biome production
-pro_plot_data$tot_land_to_atm_emiss <- pro_plot_data$ag_emiss + pro_plot_data$bg_emiss
+coup_plot_data$tot_land_to_atm_emiss <- coup_plot_data$ag_emiss + coup_plot_data$bg_emiss
 # TODO
 #check if this gets used anywhere
-pro_plot_data$npp_rh <- pro_plot_data$NPP/pro_plot_data$Rh
+coup_plot_data$npp_rh <- coup_plot_data$NPP/coup_plot_data$Rh
 
-write.csv(pro_plot_data %>% mutate(scenario='coupled'),
-          paste0(write_dir, "pro_plot_data.csv"), row.names = FALSE)
+write.csv(coup_plot_data %>% mutate(scenario='coupled'),
+          paste0(write_dir, "coup_plot_data.csv"), row.names = FALSE)
 
 
 
 # make long for plotting
-pro_plot_data_long <- pro_plot_data %>%
+coup_plot_data_long <- coup_plot_data %>%
   tidyr::pivot_longer(cols=c("land_alloc","agCDensity","bgCDensity","agCarbon",
                              "bgCarbon","NPP","Rh","litter","bg_emiss","ag_emiss","tot_land_to_atm_emiss", "npp_rh"),
                       names_to="variable",
                       values_to="value") %>%
   mutate(scenario = 'coupled') 
-rm(pro_plot_data)
+rm(coup_plot_data)
 
-write.csv(pro_plot_data_long, paste0(write_dir, "pro_plot_data_long.csv"), row.names = FALSE)
-rm(pro_plot_data_long)
+write.csv(coup_plot_data_long, paste0(write_dir, "coup_plot_data_long.csv"), row.names = FALSE)
+rm(coup_plot_data_long)
 
 
 
